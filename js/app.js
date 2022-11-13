9643aa88713b18a270caaadb610882bc68e03197
Edonisgashi <edonisgashi@outlook.com>
2022-11-13 23:20:43 +0100

    Solving some bugs ...

--- js/app.js
+++ js/app.js
@@ -17,33 +17,48 @@ const summaryTotal = document.querySelector(".summary__total");
 
 // ADDING EVENTS TO INPUT FIELDS AND UPDATING THEIR VALUES
 document.addEventListener("DOMContentLoaded", function () {
-  const prices = [];
+  const prices = [0, 0, 0, 0, 0];
 
-  inputNums.forEach(function (input) {
-    input.addEventListener("keyup", function (e) {
-      itemsArr.forEach(function (item) {
-        if (input.value.length > 0 && Number(input.value) > 0) {
-          if (input.id === item.dataset.id) {
-            item.classList.add("open");
-            // UPDATING INPUT FIELDS
-            const child = item.querySelector(".item__calc");
-            const price = item.querySelector(".item__price");
-            child.textContent = `${input.value} * $0.5`;
-            price.textContent = `  ${Number(input.value) * 0.5}`;
-            // UPDATING SUM
-            summaryTotal.classList.add("open");
-            prices.push(Number(price.textContent));
-            totalPrice.textContent = `$ ${prices.reduce(
-              (acc, el) => acc + el
-            )}`;
-          }
-        } else {
-          if (input.id === item.dataset.id) {
-            item.classList.remove("open");
-            summaryTotal.classList.remove("open");
-          }
+  products.addEventListener("keyup", function (e) {
+    itemsArr.forEach(function (item) {
+      if (products.value.length > 0 && Number(products.value) > 0) {
+        if (item.dataset.id === "products") {
+          item.classList.add("open");
+          // UPDATING INPUT FIELDS
+          const child = item.querySelector(".item__calc");
+          const price = item.querySelector(".item__price");
+          child.textContent = `${products.value} * $0.5`;
+          price.textContent = `  ${Number(products.value) * 0.5}`;
+          // UPDATING SUM
+          summaryTotal.classList.add("open");
+          prices[0] = Number(price.textContent);
+          totalPrice.textContent = `$ ${prices.reduce((acc, el) => acc + el)} `;
         }
-      });
+      } else {
+        item.classList.remove("open");
+        summaryTotal.classList.remove("open");
+      }
+    });
+  });
+  orders.addEventListener("keyup", function (e) {
+    itemsArr.forEach(function (item) {
+      if (orders.value.length > 0 && Number(orders.value) > 0) {
+        if (item.dataset.id === "orders") {
+          item.classList.add("open");
+          // UPDATING INPUT FIELDS
+          const child = item.querySelector(".item__calc");
+          const price = item.querySelector(".item__price");
+          child.textContent = `${orders.value} * $0.5`;
+          price.textContent = `  ${Number(orders.value) * 0.5}`;
+          // UPDATING SUM
+          summaryTotal.classList.add("open");
+          prices[1] = Number(price.textContent);
+          totalPrice.textContent = `$ ${prices.reduce((acc, el) => acc + el)} `;
+        }
+      } else {
+        item.classList.remove("open");
+        summaryTotal.classList.remove("open");
+      }
     });
   });
 
@@ -55,15 +70,16 @@ document.addEventListener("DOMContentLoaded", function () {
     [...options].forEach(function (option) {
       option.addEventListener("click", function () {
         itemsArr.forEach(function (item) {
-          if (option && item.dataset.id === "package") {
-            item.classList.add("open");
+          if (item.dataset.id === "package") {
+            item.classList.toggle("open");
             // UPDATING INPUT FIELDS
             const packageCalc = item.querySelector(".item__calc");
             const packagePrice = item.querySelector(".item__price");
             packageCalc.textContent = `${option.textContent}`;
             packagePrice.textContent = 36.99;
             // UPDATING SUM
-            prices.push(Number(packagePrice.textContent));
+            summaryTotal.classList.add("open");
+            prices[2] = Number(packagePrice.textContent);
             totalPrice.textContent = `$ ${prices
               .reduce((acc, el) => acc + el)
               .toFixed(2)}`;
@@ -74,14 +90,20 @@ document.addEventListener("DOMContentLoaded", function () {
   });
   accounting.addEventListener("click", function () {
     itemsArr.forEach(function (item) {
-      if (accounting && item.dataset.id === "accounting") {
-        item.classList.toggle("open");
-        // UPDATING INPUT FIELDS
-        const accountingPrice = item.querySelector(".item__price");
-        accountingPrice.textContent = 18.99;
-        // UPDATING SUM
+      if (accounting.checked && item.dataset.id === "accounting") {
+        console.log(item);
+        item.classList.add("open");
+        //  UPDATING INPUT FIELDS
         summaryTotal.classList.add("open");
-        prices.push(Number(accountingPrice.textContent));
+        const accountingPrice = item.querySelector(".item__price");
+        accountingPrice.textContent = 32.49;
+        // UPDATING SUM
+        prices[3] = Number(accountingPrice.textContent);
+        totalPrice.textContent = `$ ${prices
+          .reduce((acc, el) => acc + el)
+          .toFixed(2)}`;
+      } else {
+        prices[4] = 0;
         totalPrice.textContent = `$ ${prices
           .reduce((acc, el) => acc + el)
           .toFixed(2)}`;
@@ -90,18 +112,25 @@ document.addEventListener("DOMContentLoaded", function () {
   });
   terminal.addEventListener("click", function () {
     itemsArr.forEach(function (item) {
-      if (accounting && item.dataset.id === "terminal") {
-        item.classList.toggle("open");
+      if (terminal.checked && item.dataset.id === "terminal") {
+        console.log(item);
+        item.classList.add("open");
         //  UPDATING INPUT FIELDS
         summaryTotal.classList.add("open");
         const terminalPrice = item.querySelector(".item__price");
         terminalPrice.textContent = 25.49;
         // UPDATING SUM
-        prices.push(Number(terminalPrice.textContent));
+        prices[4] = Number(terminalPrice.textContent);
+        totalPrice.textContent = `$ ${prices
+          .reduce((acc, el) => acc + el)
+          .toFixed(2)}`;
+      } else {
+        prices[4] = 0;
         totalPrice.textContent = `$ ${prices
           .reduce((acc, el) => acc + el)
           .toFixed(2)}`;
       }
     });
   });
+  console.log(prices);
 });
